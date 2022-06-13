99cfc7f1511eb91e633c3fe6bfabd288b03000a5
Arsalan <arsalanrahman008@gmail.com>
2022-06-13 07:46:52 +0300

    pushed the chatappcode

--- lib/main.dart
+++ lib/main.dart
@@ -1,116 +1,22 @@
+import 'package:hru_chat/screens/chat_screen.dart';
+import 'package:hru_chat/screens/login_screen.dart';
+import 'package:hru_chat/screens/registration_screen.dart';
+import 'package:hru_chat/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
 
+void main() => runApp(FlashChat());
 
-
-void main() => runApp(Hru_chat());
-
-class Hru_chat extends StatelessWidget {
+class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: GirisEkrani(),
-    );
-  }
-}
-
-class GirisEkrani extends StatefulWidget {
-  @override
-  _GirisEkraniState createState() => _GirisEkraniState();
-}
-
-class _GirisEkraniState extends State<GirisEkrani> {
-  TextEditingController t1 = TextEditingController();
-  TextEditingController t2 = TextEditingController();
-
-  girisYap() {
-    if (t1.text == "admin" && t2.text == "1234") {
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => ProfilEkrani(
-            kullaniciAdi: t1.text,
-            sifre: t2.text,
-          ),
-        ),
-      );
-    } else {
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: const Text("Yanlış kullanıcı adı veya şifre"),
-            content: const Text("Lütfen giriş bilgilerinizi gözden geçirin."),
-            actions: <Widget>[
-              TextButton(
-                child: const Text("Kapat"),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-            ],
-          );
-        },
-      );
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Giriş Ekrani")),
-      body: Container(
-        margin: EdgeInsets.all(100),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextFormField(
-              decoration: const InputDecoration(hintText: "Kullanıcı Adı"),
-              controller: t1,
-            ),
-            TextFormField(
-              decoration: const InputDecoration(hintText: "Şifre"),
-              controller: t2,
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  girisYap();
-                },
-                child: const Text("Giriş Yap")),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class ProfilEkrani extends StatefulWidget {
-  String kullaniciAdi, sifre;
-  ProfilEkrani({required this.kullaniciAdi, required this.sifre});
-
-  @override
-  _ProfilEkraniState createState() => _ProfilEkraniState();
-}
-
-class _ProfilEkraniState extends State<ProfilEkrani> {
-  cikisYap() {
-    Navigator.pop(context);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: cikisYap,
-              child: Text("Çıkış Yap"),
-            ),
-            Text("Kullanıcı Adınız: ${widget.kullaniciAdi}"),
-            Text("Şifreniz: ${widget.sifre}"),
-          ],
-        ),
-      ),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatScreen.id: (context) => ChatScreen()
+      },
     );
   }
 }
---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,177 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:hru_chat/constants.dart';
+import 'package:flutter/material.dart';
+
+final _firestore = FirebaseFirestore.instance;
+User loggedInUser;
+
+class ChatScreen extends StatefulWidget {
+  static const String id = 'chat_screen';
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  void initState() {
+    getCurrentUser();
+    super.initState();
+  }
+
+  final messageController = TextEditingController();
+  final _auth = FirebaseAuth.instance;
+  String messageText;
+
+  void getCurrentUser() {
+    try {
+      final user = _auth.currentUser;
+      if (user != null) {
+        loggedInUser = user;
+        print(loggedInUser);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: null,
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.close),
+              onPressed: () {
+                _auth.signOut();
+                Navigator.pop(context);
+              }),
+        ],
+        title: Text('⚡️Chat'),
+        backgroundColor: Colors.lightBlueAccent,
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            MessageStream(),
+            Container(
+              decoration: kMessageContainerDecoration,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      controller: messageController,
+                      onChanged: (value) {
+                        messageText = value;
+                      },
+                      decoration: kMessageTextFieldDecoration,
+                    ),
+                  ),
+                  FlatButton(
+                    onPressed: () {
+                      messageController.clear();
+                      _firestore.collection('messages').add(
+                          {'text': messageText, 'sender': loggedInUser.email});
+                    },
+                    child: Text(
+                      'Send',
+                      style: kSendButtonTextStyle,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MessageStream extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _firestore.collection('messages').snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          final messages = snapshot.data.docs.reversed;
+          List<MessageBubble> messagesWidget = [];
+          for (var message in messages) {
+            final messageText = message.data()['text'];
+            final messageSender = message.data()['sender'];
+            final currentUser = loggedInUser.email;
+            if (messageText != null) {
+              final messageWidget = MessageBubble(
+                sender: messageSender,
+                text: messageText,
+                isMe: currentUser == messageSender,
+              );
+              messagesWidget.add(messageWidget);
+            }
+          }
+          return Expanded(
+            child: ListView(
+              reverse: true,
+              padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+              children: messagesWidget,
+            ),
+          );
+        } else {
+          return Center(
+            child: CircularProgressIndicator(
+              backgroundColor: Colors.lightBlueAccent,
+            ),
+          );
+        }
+      },
+    );
+  }
+}
+
+class MessageBubble extends StatelessWidget {
+  final String sender;
+  final String text;
+  final bool isMe;
+  MessageBubble({this.sender, this.text, this.isMe});
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment:
+            isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+        children: [
+          Text(
+            '$sender',
+            style: TextStyle(color: Colors.black54, fontSize: 12.0),
+          ),
+          Material(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(30.0),
+              topLeft: isMe ? Radius.circular(30.0) : Radius.circular(0.0),
+              bottomRight: Radius.circular(30.0),
+              topRight: isMe ? Radius.circular(0.0) : Radius.circular(30.0),
+            ),
+            elevation: 5.0,
+            color: isMe ? Colors.lightBlueAccent : Colors.white,
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              child: Text(
+                '$text',
+                style: TextStyle(
+                    fontSize: 15.0,
+                    color: isMe ? Colors.white : Colors.black54),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
