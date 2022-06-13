c23cd92fe752ca19c19919a53795154b7b90d7a4
gule-fadil <gulekhalil453@gmail.com>
2022-06-13 10:51:04 +0300

    add firebase core-cloud-auth to yaml

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,9 @@ import 'package:hru_chat/screens/registration_screen.dart';
 import 'package:hru_chat/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(FlashChat());
+void main() => runApp(hru_chat());
 
-class FlashChat extends StatelessWidget {
+class hru_chat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
