38b38593516a651ef5479e5e7ce9f2871a7dafad
gule-fadil <gulekhalil453@gmail.com>
2022-06-13 03:04:05 +0300

    mesajlasma

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
 
+
+
 void main() => runApp(Hru_chat());
 
 class Hru_chat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: "/",
-      routes: {
-        "/": (context) => GirisEkrani(),
-        "/ProfilSayfasiRotasi": (context) => ProfilEkrani(),
-      },
+      home: GirisEkrani(),
     );
   }
 }
+
 class GirisEkrani extends StatefulWidget {
   @override
   _GirisEkraniState createState() => _GirisEkraniState();
@@ -24,21 +23,26 @@ class _GirisEkraniState extends State<GirisEkrani> {
   TextEditingController t2 = TextEditingController();
 
   girisYap() {
-    if (t1.text== "admin"&& t2.text=="1234"){Navigator.pushNamed(
-      context,
-      "/ProfilSayfasiRotasi",
-      arguments: VeriModeli(kullaniciAdi: t1.text, sifre: t2.text),
-    );
+    if (t1.text == "admin" && t2.text == "1234") {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => ProfilEkrani(
+            kullaniciAdi: t1.text,
+            sifre: t2.text,
+          ),
+        ),
+      );
     } else {
       showDialog(
         context: context,
         builder: (BuildContext context) {
           return AlertDialog(
-            title: new Text("Yanlış kullanıcı adı veya şifre"),
-            content: new Text("Lütfen giriş bilgilerinizi gözden geçirin."),
+            title: const Text("Yanlış kullanıcı adı veya şifre"),
+            content: const Text("Lütfen giriş bilgilerinizi gözden geçirin."),
             actions: <Widget>[
-              new FlatButton(
-                child: new Text("Kapat"),
+              TextButton(
+                child: const Text("Kapat"),
                 onPressed: () {
                   Navigator.of(context).pop();
                 },
@@ -46,7 +50,8 @@ class _GirisEkraniState extends State<GirisEkrani> {
             ],
           );
         },
-      );}
+      );
+    }
   }
 
   @override
@@ -54,19 +59,23 @@ class _GirisEkraniState extends State<GirisEkrani> {
     return Scaffold(
       appBar: AppBar(title: Text("Giriş Ekrani")),
       body: Container(
+        margin: EdgeInsets.all(100),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             TextFormField(
+              decoration: const InputDecoration(hintText: "Kullanıcı Adı"),
               controller: t1,
             ),
             TextFormField(
+              decoration: const InputDecoration(hintText: "Şifre"),
               controller: t2,
             ),
-            RaisedButton(
+            ElevatedButton(
                 onPressed: () {
                   girisYap();
                 },
-                child: Text("Giriş Yap")),
+                child: const Text("Giriş Yap")),
           ],
         ),
       ),
@@ -75,6 +84,9 @@ class _GirisEkraniState extends State<GirisEkrani> {
 }
 
 class ProfilEkrani extends StatefulWidget {
+  String kullaniciAdi, sifre;
+  ProfilEkrani({required this.kullaniciAdi, required this.sifre});
+
   @override
   _ProfilEkraniState createState() => _ProfilEkraniState();
 }
@@ -87,23 +99,18 @@ class _ProfilEkraniState extends State<ProfilEkrani> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Profil Sayfası")),
       body: Container(
         child: Column(
           children: <Widget>[
-            RaisedButton(
+            ElevatedButton(
               onPressed: cikisYap,
               child: Text("Çıkış Yap"),
             ),
-
+            Text("Kullanıcı Adınız: ${widget.kullaniciAdi}"),
+            Text("Şifreniz: ${widget.sifre}"),
           ],
         ),
       ),
     );
   }
-}
-
-class VeriModeli {
-  String kullaniciAdi, sifre;
-  VeriModeli({required this.kullaniciAdi, required this.sifre});
 }
